761b31cbfb0a4d8836b8ba3da3b4caec4a1c06d1
VVLSpaceship <VVLSpaceship@proton.me>
2026-07-21 17:39:47 -1000

    Delete Bot/src/commands/commands directory

--- Bot/src/commands/commands/guildlist.d.ts
+++
@@ -1,4 +0,0 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
-export declare const data: SlashCommandBuilder;
-export declare function execute(interaction: ChatInputCommandInteraction, db: any): Promise<void>;
-//# sourceMappingURL=guildlist.d.ts.map
